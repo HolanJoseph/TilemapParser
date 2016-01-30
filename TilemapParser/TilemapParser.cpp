cdb81f83c76349cd971750498c32c6144631baab
Joseph Holan <holanjoseph@gmail.com>
2016-01-30 02:54:38 -0600

    finished write bmp, messing with memory manip

--- TilemapParser/TilemapParser.cpp
+++ TilemapParser/TilemapParser.cpp
@@ -90,8 +90,52 @@ struct BMPPaletteElement
 
 #define return Sleep(5000); return
 
+u32 padVal(u32 x, u32 padTo)
+{
+	u32 retval = (x + padTo) & ((u32)0-padTo);
+	return retval;
+}
+
+typedef u32 tile[256];
+
 i32 main(i32 numArguments, char** arguments)
 {
+
+
+	tile x = { 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x22222222, 
+		0x11111111, 0x22222222, 0x11111111, 0x22222222, 0x11111111, 0x99999999
+	};
+	
+	u32 (*x2)[256] = (u32(*)[256])malloc(sizeof(tile));
+	for (u64 i = 0; i < 256; ++i)
+	{
+		(*x2)[i] = x[i];
+	}
+
 	char* tilemapFilename = NULL;
 	
 	u32 screenWidth = 0;
@@ -100,7 +144,7 @@ i32 main(i32 numArguments, char** arguments)
 	Tile tmTile;
 	tmTile.width = 0;
 	tmTile.height = 0;
-	
+
 	Offsets tmOffsets;
 	tmOffsets.left = 0;
 	tmOffsets.right = 0;
@@ -134,7 +178,13 @@ i32 main(i32 numArguments, char** arguments)
 		return 0;
 	}
 
-	HANDLE tilemapHandle = CreateFileA(
+
+	// read in .bmp file
+	// open file
+	// read in bmp header to get file size
+	// create a buffer to hold the data
+	// close the file
+	HANDLE tmHandle = CreateFileA(
 										tilemapFilename,
 										GENERIC_READ,
 										(FILE_SHARE_READ | FILE_SHARE_WRITE),
@@ -143,24 +193,24 @@ i32 main(i32 numArguments, char** arguments)
 										FILE_ATTRIBUTE_NORMAL,
 										NULL
    									);
-	if (tilemapHandle == INVALID_HANDLE_VALUE)
+	if (tmHandle == INVALID_HANDLE_VALUE)
 	{
 		cout << "Specified file could not be opened" << endl;
 		return 0;
 	}
 
-	BMPFileHeader tilemapFileHeader;
-	BMPDataHeader tilemapDataHeader;
+	BMPFileHeader tmFileHeader;
+	BMPDataHeader tmDataHeader;
 	DWORD numBytesRead = 0;
 
 	BOOL fileHeaderRead = ReadFile(
-		tilemapHandle,
-		&tilemapFileHeader,
-		sizeof(tilemapFileHeader),
+		tmHandle,
+		&tmFileHeader,
+		sizeof(tmFileHeader),
 		&numBytesRead,
 		NULL
 		);
-	if (fileHeaderRead == FALSE || (numBytesRead != sizeof(tilemapFileHeader)))
+	if (fileHeaderRead == FALSE || (numBytesRead != sizeof(tmFileHeader)))
 	{
 		cout << "Failed to read .bmp file header" << endl;
 		return 0;
@@ -170,25 +220,24 @@ i32 main(i32 numArguments, char** arguments)
 
 	numBytesRead = 0;
 	BOOL dataHeaderRead = ReadFile(
-		tilemapHandle,
-		&tilemapDataHeader,
-		sizeof(tilemapDataHeader),
+		tmHandle,
+		&tmDataHeader,
+		sizeof(tmDataHeader),
 		&numBytesRead,
 		NULL
 		);
-
-	if (dataHeaderRead == FALSE || (numBytesRead != sizeof(tilemapDataHeader)))
+	if (dataHeaderRead == FALSE || (numBytesRead != sizeof(tmDataHeader)))
 	{
 		cout << "Failed to read .bmp data header" << endl;
 		return 0;
 	}
 
-	u32 numColorPaletteEntries = (tilemapFileHeader.BitmapOffset - sizeof(tilemapFileHeader)-sizeof(tilemapDataHeader)) / sizeof(BMPPaletteElement); //1 << tilemapDataHeader.BitsPerPixel;
-	BMPPaletteElement* tilemapColorPalette = new BMPPaletteElement[numColorPaletteEntries];
+	u32 numColorPaletteEntries = (tmFileHeader.BitmapOffset - sizeof(tmFileHeader)-sizeof(tmDataHeader)) / sizeof(BMPPaletteElement); //1 << tilemapDataHeader.BitsPerPixel;
+	BMPPaletteElement* tmColorPalette = new BMPPaletteElement[numColorPaletteEntries];
 	numBytesRead = 0;
 	BOOL colorPaletteRead = ReadFile(
-		tilemapHandle,
-		tilemapColorPalette,
+		tmHandle,
+		tmColorPalette,
 		sizeof(BMPPaletteElement)*numColorPaletteEntries,
 		&numBytesRead,
 		NULL
@@ -198,15 +247,71 @@ i32 main(i32 numArguments, char** arguments)
 		cout << "Failed to read .bmp color palette" << endl;
 		return 0;
 	}
+	//cout << "tilemap is: " << tilemapDataHeader.Width << " x " << tilemapDataHeader.Height << endl;
 
-	cout << "tilemap is: " << tilemapDataHeader.Width << " x " << tilemapDataHeader.Height << endl;
+
+	u64 tmIndexedDataWidth = tmDataHeader.Width;
+	u64 tmIndexedDataHeight = tmDataHeader.Height;
+	u8* tmIndexedData = (u8*)malloc(tmDataHeader.SizeOfBitmap); // (tilemapDataHeader.BitsPerPixel / 8)*tilemapDataHeader.Width*tilemapDataHeader.Height);
+	numBytesRead = 0;
+	BOOL bitmapRead = ReadFile(
+		tmHandle,
+		tmIndexedData,
+		tmDataHeader.SizeOfBitmap,
+		&numBytesRead,
+		NULL
+		);
+	if (bitmapRead == FALSE || (numBytesRead != tmDataHeader.SizeOfBitmap))
+	{
+		cout << "Failed to read .bmp bitmap indexed data" << endl;
+		return 0;
+	}
+
+	// Convert to rgba values
+	// bottom up
+	// bgra
+// 	   (0xbb << 0 |
+// 		0x66 << 8 |
+// 		0x55 << 16 |
+// 		0xaa << 24);
+	// working on the indexed data which has different indices from the non indexed data
+	u64 tmBGRADataWidth = (tmDataHeader.Width - tmOffsets.right - tmOffsets.left);
+	u64 tmBGRADataHeight = (tmDataHeader.Height - tmOffsets.top - tmOffsets.bottom);
+	u32* tmBGRAData = (u32*)malloc(sizeof(u32)*tmBGRADataWidth * tmBGRADataHeight);
+	
+	u64 numRows = tmDataHeader.Height;
+	u64 numColumns = tmDataHeader.Width;
+	for (u64 indexedRow = tmOffsets.bottom, row=0; indexedRow < numRows - tmOffsets.top; ++indexedRow, ++row)
+	{
+		for (u64 indexedColumn = tmOffsets.left, column = 0; indexedColumn < numColumns - tmOffsets.right; ++indexedColumn, ++column)
+		{
+			u8 paletteIndex = tmIndexedData[indexedRow*tmIndexedDataWidth + indexedColumn];
+			BMPPaletteElement pe = tmColorPalette[paletteIndex];
+			tmBGRAData[row*tmBGRADataWidth + column] = (pe.Blue << 0 |
+														pe.Green << 8 |
+														pe.Red << 16 |
+														0xff << 24);
+		}
+	}
+
+	CloseHandle(tmHandle);
 
 
-	// read in .bmp file
-		// open file
-		// read in bmp header to get file size
-		// create a buffer to hold the data
-		// close the file
+	u64 sizeOfTile = sizeof(u32)* tmTile.width * tmTile.height;
+	u64 numTileColumns = tmBGRADataWidth / tmTile.width;
+	u64 numTileRows = tmBGRADataHeight / tmTile.height;
+	u32* tilesMem = (u32*)malloc(sizeOfTile * numTileColumns * numTileRows);
+
+
+	for (u64 row = 0; row < numTileRows; ++row)
+	{
+		for (u64 column = 0; column < numTileColumns; ++column)
+		{
+
+		}
+	}
+
+
 	// cut it up into screens
 		// subtract the offsets 
 		// divide by screen size
@@ -224,9 +329,93 @@ i32 main(i32 numArguments, char** arguments)
 		// if the unique tile list isnt empty
 			// create a new folder
 			// inside the folder for each tile in the unique tile list create a new .bmp file with its pixel data
-			
 
-	CloseHandle(tilemapHandle);
+
+	// Test create bmp
+// 	char* dirPath = "C:/tilemap_tiles";
+// 	BOOL dir = CreateDirectoryA( dirPath, NULL ); // only creates the directory if it doesn't exist
+
+	// Create output file
+	char* outfilePath = "testout.bmp";
+	HANDLE outfileHandle = CreateFile(
+		outfilePath,
+		GENERIC_WRITE,
+		0,
+		NULL,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL
+		);
+	if (outfileHandle == INVALID_HANDLE_VALUE)
+	{
+		cout << "Could not output individual tile bmp files" << endl;
+		return 0;
+	}
+
+	BMPFileHeader outfileFileHeader = { 0 };
+	outfileFileHeader.FileType = 0x4D42;
+	outfileFileHeader.FileSize = sizeof(BMPFileHeader)+sizeof(BMPDataHeader) + (sizeof(u32)*tmBGRADataWidth*tmBGRADataHeight);
+	outfileFileHeader.Reserved1 = 0;
+	outfileFileHeader.Reserved2 = 0;
+	outfileFileHeader.BitmapOffset = sizeof(BMPFileHeader) + sizeof(BMPDataHeader);
+
+	BMPDataHeader outfileDataHeader = { 0 };
+	outfileDataHeader.Size = 108;
+	outfileDataHeader.Width = tmBGRADataWidth;
+	outfileDataHeader.Height = tmBGRADataHeight;
+	outfileDataHeader.Planes = 1;
+	outfileDataHeader.BitsPerPixel = 32;
+	outfileDataHeader.Compression = 0;
+	outfileDataHeader.SizeOfBitmap = 0;
+	outfileDataHeader.HorzResolution = tmDataHeader.HorzResolution;
+	outfileDataHeader.VertResolution = tmDataHeader.VertResolution;
+	outfileDataHeader.ColorsUsed = 0;
+	outfileDataHeader.ColorsImportant = 0;
+	outfileDataHeader.RedMask = 0x00ff0000;
+	outfileDataHeader.GreenMask = 0x0000ff00;
+	outfileDataHeader.BlueMask = 0x000000ff;
+	outfileDataHeader.AlphaMask = 0xff000000;
+	outfileDataHeader.CSType = 0x01;
+	outfileDataHeader.RedX = 0;
+	outfileDataHeader.RedY = 0;
+	outfileDataHeader.RedZ = 0;
+	outfileDataHeader.GreenX = 0;
+	outfileDataHeader.GreenY = 0;
+	outfileDataHeader.GreenZ = 0;
+	outfileDataHeader.BlueX = 0;
+	outfileDataHeader.BlueY = 0;
+	outfileDataHeader.BlueZ = 0;
+	outfileDataHeader.GammaRed = 0;
+	outfileDataHeader.GammaGreen = 0;
+	outfileDataHeader.GammaBlue = 0;
+
+	DWORD numBytesWritten = 0;
+	BOOL ofhw = WriteFile(
+		outfileHandle,
+		&outfileFileHeader,
+		sizeof(outfileFileHeader),
+		&numBytesWritten,
+		NULL
+		);
+
+	BOOL odhw = WriteFile(
+		outfileHandle,
+		&outfileDataHeader,
+		sizeof(outfileDataHeader),
+		&numBytesWritten,
+		NULL
+		);
+
+	BOOL odw = WriteFile(
+		outfileHandle,
+		tmBGRAData,
+		sizeof(u32) * tmBGRADataWidth * tmBGRADataHeight,
+		&numBytesWritten,
+		NULL
+		);
+
+
+	CloseHandle(outfileHandle);
 	Sleep(5000);
 	return 0;
 }
